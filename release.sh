5a4d84eb277cef7fa6ae3da0d9c0b43a0b0c3684
FitGD Dev <dev@fitgd.local>
2025-12-22 14:13:39 +0000

    chore(scripts): Update release script to accept version argument

--- release.sh
+++ release.sh
@@ -5,8 +5,12 @@
 CURRENT_VERSION=$(jq -r '.version' foundry/system.json)
 echo "Current version: $CURRENT_VERSION"
 
-# Prompt for new version
-read -p "New version: " NEW_VERSION
+# Check for argument or prompt for new version
+if [ -n "$1" ]; then
+  NEW_VERSION="$1"
+else
+  read -p "New version: " NEW_VERSION
+fi
 
 # Update system.json
 jq ".version = \"$NEW_VERSION\"" foundry/system.json > tmp.json && mv tmp.json foundry/system.json
